c42c455e19641771d9063bdc74f05c4e7b1f0888
Abdullah Aydoğan <aydoganabdullah34@gmail.com>
2023-09-18 18:20:30 +0300

    yeni sayfa eklendi

--- js/app.js
+++ js/app.js
@@ -92,6 +92,27 @@ $(document).ready(function() {
         }
     });
 
+    $('.med-section .slider-area').owlCarousel({
+        loop: false,
+        margin: 30,
+        dots: true,
+        dotsEach: 1,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            600: {
+                items: 1
+            },
+
+            1000: {
+                items: 1
+            }
+        }
+    });
+
     function postsCarousel() {
 
         var checkWidth = $(window).width();
